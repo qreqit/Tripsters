7e5fa5b5ccd18f441a165bedb49c3c5aa0b07695
greqit <greqit@gmail.com>
2024-10-07 09:48:40 +0200

    add all fields with id for work

--- src/main/java/com/example/tripsters/model/Chat.java
+++ src/main/java/com/example/tripsters/model/Chat.java
@@ -1,4 +1,15 @@
 package com.example.tripsters.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "chats")
 public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
