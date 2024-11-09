7ca98d944974b4aaaa88d0c9846753b8cf846455
greqit <greqit@gmail.com>
2024-11-09 22:00:25 +0100

    refactor security

--- src/main/java/com/example/tripsters/config/SecurityConfig.java
+++ src/main/java/com/example/tripsters/config/SecurityConfig.java
@@ -31,7 +31,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
