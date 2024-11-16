72e087effcfbbdee2d1e3f3b42ffc220f2560160
greqit <greqit@gmail.com>
2024-11-16 10:45:36 +0100

    add functionality to show current user

--- src/main/java/com/example/tripsters/controller/UserController.java
+++ src/main/java/com/example/tripsters/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.tripsters.dto.user.UserResponseDto;
 import com.example.tripsters.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,14 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping
+    @GetMapping("/all")
     @PreAuthorize("hasRole('ADMIN')")
     public List<UserResponseDto> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/current")
+    public UserResponseDto getCurrentUser(Authentication authentication) {
+        return userService.getCurrentUser(authentication);
     }
 }
