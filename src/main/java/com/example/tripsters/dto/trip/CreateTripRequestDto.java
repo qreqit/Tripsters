6c7fbee68d21d1c7f88f77d28efe40f9c0d4b248
greqit <greqit@gmail.com>
2024-11-18 16:42:11 +0100

    fix

--- src/main/java/com/example/tripsters/dto/trip/CreateTripRequestDto.java
+++ src/main/java/com/example/tripsters/dto/trip/CreateTripRequestDto.java
@@ -1,8 +1,6 @@
 package com.example.tripsters.dto.trip;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import java.util.List;
 
@@ -18,7 +16,5 @@ public class CreateTripRequestDto {
     private String startPoint;
     @NotBlank
     private String endPoint;
-    @NotNull
-    @NotEmpty
     private List<String> additionalPoints;
 }
