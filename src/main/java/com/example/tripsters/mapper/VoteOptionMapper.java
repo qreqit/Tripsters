5fdc57e59c5db1c9a7941164427e7d94edd82672
greqit <greqit@gmail.com>
2025-01-22 11:32:23 +0200

    fix counts changes

--- src/main/java/com/example/tripsters/mapper/VoteOptionMapper.java
+++ src/main/java/com/example/tripsters/mapper/VoteOptionMapper.java
@@ -4,8 +4,10 @@ import com.example.tripsters.config.MapperConfig;
 import com.example.tripsters.dto.vote.VoteOptionResponseDto;
 import com.example.tripsters.model.VoteOption;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class)
 public interface VoteOptionMapper {
+    @Mapping(source = "voteCount", target = "count")
     VoteOptionResponseDto toDto(VoteOption voteOption);
 }
